be0ada2b2d7bd8782de66225734d6e221b3a414b
Premchand007 <premchandthumatinitb@gmail.com>
2020-07-26 11:11:29 +0530

    Topo

---
+++ Topological Sort.cpp
@@ -0,0 +1,47 @@
+/*
+Topological sorting for Directed Acyclic Graph (DAG) is a 
+linear ordering of vertices such that for every directed edge uv, 
+vertex u comes before v in the ordering.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> adj[10000];
+vector<bool> visited;
+vector<int> ans;
+
+void dfs(int u) {
+    visited[u] = true;
+    for(int v : adj[u]) {
+        if(!visited[v]) dfs(v);
+    }
+    ans.push_back(u);
+}
+
+void topological_sort(int n) {
+    visited.assign(10000, false);
+    ans.clear();
+
+    for(int i = 0; i < n; ++i) {
+        if(!visited[i]) dfs(i);
+    }
+    reverse(ans.begin(), ans.end());
+}
+
+int main() {
+
+    int n, m;
+    cin >> n >> m;
+
+    while(m--) {
+        int a, b;
+        cin >> a >> b;
+        // --a, --b; If index is 1 to n
+        adj[a].push_back(b);
+    }
+
+    topological_sort(n);
+    for(int i = 0; i < n; ++i) cout << ans[i] << " "; // ans[i] + 1 if index is 1 to n
+    cout << endl;
+    
+}
