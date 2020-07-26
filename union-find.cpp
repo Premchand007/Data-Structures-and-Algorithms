ae6a95b971726eddd7689b2b41981dcaf841afff
Premchand007 <premchandthumatinitb@gmail.com>
2020-07-26 10:55:32 +0530

    Comments

--- union-find.cpp
+++ union-find.cpp
@@ -10,9 +10,9 @@ int find(int x, int par[]) {
 int main() {
     
     int n, m;
-    cin >> n >> m;
+    cin >> n >> m; //n vertices, m edges
 
-    int par[n], rank[n], size[n];
+    int par[n], rank[n], size[n]; 
     for(int i = 0; i < n; ++i) {
         par[i] = i, rank[i] = 0, size[i] = 1;
     }
@@ -22,10 +22,11 @@ int main() {
         cin >> a >> b;
 
 
-        a = find(a, par);
+        a = find(a, par); // returns parent of set in which a is present
         b = find(b, par);
         if(a == b) continue;
 
+        // If both a and b are in different sets the union of two sets is done.
         if(rank[a] < rank[b]) {
             par[a] = b;
             size[b] += size[a];
